218fd81928f316e99a85d5346fdecc4060dbec58
Nuwan Janaka <nuwanjanakan.p.k@gmail.com>
2018-04-02 11:27:25 +0530

    Added Typed.Enum types

--- src/app/models/Model.js
+++ src/app/models/Model.js
@@ -4,3 +4,26 @@ import {Any, List, Maybe, Record, Typed, Union} from 'typed-immutable';
 export {Map, fromJS};
 
 export {Maybe, Any, List, Record, Typed, Union};
+
+Typed.Enum = (types) => {
+  if (typeof(types) !== 'object') {
+    throw TypeError(`${types} is not a object`);
+  }
+
+  const keys = Object.keys(types);
+  const TypedEnum = Typed(`Typed.Enum(${keys.join(', ')})`, value => {
+
+    if (value && keys.includes(value)) {
+      return value;
+    }
+    return TypeError(`${value} should be in ${keys}`);
+  });
+
+  keys.forEach(
+    (key) => TypedEnum[key] = key
+  );
+
+  return TypedEnum;
+};
+
+
---
+++ src/app/models/Model.spec.js
@@ -0,0 +1,23 @@
+
+import {Typed, Record} from './Model';
+
+describe('Typed.Enum', () => {
+
+  it('should support typed Enum', () => {
+
+    const Directions = Typed.Enum({
+      NORTH:'North',
+      EAST:'East',
+      WEST:'West',
+      SOUTH: 'South'
+    });
+
+    const TestRecord = Record({
+      value: Directions
+    });
+
+    expect(TestRecord({value: 'NORTH'})).toEqual(TestRecord({value: Directions.NORTH}));
+    expect(TestRecord({value: 'North'})).toEqual(TypeError('North should be in NORTH,EAST,WEST,SOUTH'));
+  });
+
+});
